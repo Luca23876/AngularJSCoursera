e7e0ac2ead9baa9e0655b1e9302124323528ffb8
Luca23876 <luca.ciancanelli1785@gmail.com>
2017-08-05 01:05:14 -0600

    mod2 solution found

--- mod2-solution/app.js
+++ mod2-solution/app.js
@@ -1,18 +1,66 @@
-()(function(){
-module.angular("shoppingListCheckOff", shoppingListCheckOff)
+(function () {
+  'use strict';
 
-.controller("toBuyList", toBuyList)
-.controller("boughtList",boughtList)
-.service("ShoppingListCheckOffService",ShoppingListCheckOffService)
+  var data = [];
+  var shoppingList = [
+    {
+      name: "Donuts",
+      quantity: "10"
+    },
+    {
+    name: "Cookies",
+    quantity: "10"
+    },
+    {
+    name: "drinks",
+    quantity: "10"
+    },
+    {
+    name: "shrimp",
+    quantity: "10"
+    },
+    {
+    name: "blah",
+    quantity: "100"
+    }
+  ];
+  console.log(data);
+  angular.module('shoppingListCheckOffApp', [])
+  .controller('toBuyListController', toBuyListController)
+  .controller('boughtListController', boughtListController)
+  .service('shoppingListService', shoppingListService);
 
-toBuyList.$inject(ShoppingListCheckOffService, '$scope');
-boughtList.$inject(ShoppingListCheckOffService, '$scope');
-function toBuyList(ShoppingListCheckOffService, $scope){
+  toBuyListController.$inject = ['shoppingListService'];
+  function toBuyListController(shoppingListService){
+    var buy = this;
+    buy.shoppingList = shoppingList;
+    buy.shoppingListBought = function(itemIndex){
+      shoppingListService.dataTransfer(buy.shoppingList[itemIndex].name, buy.shoppingList[itemIndex].quantity);
+      shoppingListService.remove(itemIndex);
+    };
+  }
+  boughtListController.inject = ['shoppingListService'];
+  function boughtListController(shoppingListService){
+    var bought = this;
+    bought.data = shoppingListService.getData();
+    console.log(bought.data);
+  }
 
+  function shoppingListService(){
+    var service = this;
+    service.dataTransfer = function(itemName, quantity){
+      var item = {
+        name: itemName,
+        quantity: quantity
+      };
+      data.push(item);
+    }
+    service.remove = function (itemIndex) {
+      shoppingList.splice(itemIndex, 1);
+    };
+    service.getData = function() {
+      return data;
+    };
 
-};
-function boughtList(ShoppingListCheckOffService, $scope){
-$scope.boughtItems = []
-
-};
-}):
+  };
+})();
