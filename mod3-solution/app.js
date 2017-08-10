ab07799ec3f966bb33def5cdf7fa98284537c264
Amon22B <luca.ciancanelli1785@gmail.com>
2017-08-09 23:39:35 -0600

    starter code

---
+++ mod3-solution/app.js
@@ -0,0 +1,13 @@
+(function(){
+  'use strict'
+
+  angular.module('narrowDownMenuApp', [])
+  .controller('searchController', searchController)
+  function searchController() {
+    var menu = this;
+    menu.input = "";
+    menu.search = function() {
+      
+    }
+  }
+})();
