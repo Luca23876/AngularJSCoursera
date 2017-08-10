0f854bfa9dc35961da930bf8ec9513fe767559bd
Amon22B <luca.ciancanelli1785@gmail.com>
2017-08-10 12:24:24 -0600

    progress

--- mod3-solution/app.js
+++ mod3-solution/app.js
@@ -2,12 +2,31 @@
   'use strict'
 
   angular.module('narrowDownMenuApp', [])
-  .controller('searchController', searchController)
-  function searchController() {
+  .controller('narrowItDownController ', narrowItDownController )
+  .service('MenuSearchService', MenuSearchService);
+
+  narrowItDownController.$inject = ['MenuSearchService'];
+  function narrowItDownController(MenuSearchService) {
     var menu = this;
     menu.input = "";
     menu.search = function() {
-      
+      MenuSearchService.getMatchedMenuItems(searchTerm);
+    }
+  }
+  MenuSearchService.$inject = ['$https'];
+  function MenuSearchService($https) {
+    var service = this;
+    service.getMatchedMenuItems = function(searchTerm) {
+      return $http({
+        method: "GET",
+        url: ('https://davids-restaurant.herokuapp.com/menu_items.json')
+      }).then(function (result) {
+          console.log(result);
+        var foundItems
+
+        // return processed items
+        return foundItems;
+      });
     }
   }
 })();
