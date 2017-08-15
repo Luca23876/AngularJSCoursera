39e54a521a7db331537e49e089fb14288e0ecef5
Luca23876 <luca.ciancanelli1785@gmail.com>
2017-08-15 12:27:46 -0600

    progress

--- mod3-solution/app.js
+++ mod3-solution/app.js
@@ -4,22 +4,37 @@
   angular.module('narrowDownMenuApp', [])
   .controller('narrowItDownController', narrowItDownController)
   .service('MenuSearchService', MenuSearchService);
+  .directive('searchResult', searchResultDirective);
+
+  function searchResultDirective() {
+    var ddo = {
+      templateUrl: 'searchResult.html'
+    };
+
+    return ddo
+  }
 
   narrowItDownController.$inject = ['MenuSearchService'];
   function narrowItDownController(MenuSearchService) {
     var menu = this;
     menu.input = "";
+    menu.message = "";
+    menu.foundArray = [];
     menu.searchX = function(name) {
       MenuSearchService.getMatchedMenuItems(name, menu.input);
+      MenuSearchService.getFoundItems(menu.foundArray, menu.message);
     };
-    //var promise = MenuSearchService.getFoundItems();
+    var promise = MenuSearchService.getMatchedMenuItems();
   }
-  MenuSearchService.$inject = ['$http'];
-  function MenuSearchService($http) {
+
+
+
+  MenuSearchService.$inject = ['$http', '$q'];
+  function MenuSearchService($http, $q) {
     var service = this;
     service.getMatchedMenuItems = function(name, searchTerm) {
+      var deferred = $q.defer();
       var foundItems = [];
-      var enterSearch = "Please enter search Term";
       var result =  $http({
         method: "GET",
         url: ('https://davids-restaurant.herokuapp.com/menu_items.json'),
@@ -30,21 +45,30 @@
           var items = result.data;
           for (var i = 0; i < items.menu_items.length; i++) {
             if (searchTerm === ""){
-              console.log("Please enter search Term");
+              result.message = "Please enter search term"
+              deferred.reject(result);
               i = items.menu_items.length;
             }
             else if (items.menu_items[i].name.toLowerCase().indexOf(searchTerm.toLowerCase()) ==! -1){
                 foundItems.push(items.menu_items[i].name)
-                console.log(foundItems);
+                deferred.resolve(foundItems);
             }else {
               console.log("doesn't match search");
             }
           };
 
       });
+      return deferred.promise;
     }
-    // service.getFoundItems = function () {
-    //   return foundItems;
-    // }
-  }
+    service.getFoundItems = function (foundArray, message) {
+      var searchResult = MenuSearchService.getMatchedMenuItems(name, searchTerm);
+      $q.all([searchResult]).
+      then(function (foundItems) {
+        var foundArray = foundItems;
+      }).
+      catch(function (errorResponse) {
+        var message = errorResponse.message;
+      });
+    };
+  };
 })();
