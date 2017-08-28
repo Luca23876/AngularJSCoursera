6d5129dd1cfd606f8c1b4209c4d0223e70468f71
Luca23876 <luca.ciancanelli1785@gmail.com>
2017-08-27 22:59:25 -0600

    Code clean up and Search Error Fix

--- mod3-solution/app.js
+++ mod3-solution/app.js
@@ -20,38 +20,24 @@
 
   narrowItDownController.$inject = ['MenuSearchService'];
   function narrowItDownController(MenuSearchService) {
-    var menu = this;
-    menu.page = [];
-    menu.nPage = [];
-    menu.input = "";
-    menu.displayResult = [];
-    menu.searchX = function(name) {
-      menu.displayResult = MenuSearchService.FoundItems(menu.input, name);
-      menu.displayResult.$promise
-      .then(function(foundArray) {
-        menu.page = foundArray;
-        menu.nPage = [];
-        return menu.page;
-      }).catch(function(errorResponse) {
-        console.log("ERROR");
-        menu.nPage = errorResponse;
-        menu.page = [];
-        return menu.nPage;
-      });
+      var menu = this;
+      menu.input = "";
       menu.removeItem;
-    };
-    menu.removeItem = function (itemIndex, items) {
-      MenuSearchService.removeItem(itemIndex, menu.page);
-    };
-  }
+      menu.searchX = function(name) {
+        menu.displayResult = MenuSearchService.getMatchedMenuItems(menu.input, name);
+        return menu.displayResult;
+      };
+      menu.removeItem = function (itemIndex) {
+        MenuSearchService.removeItem(itemIndex, menu.displayResult);
+      };
+  };
 
 
 
   MenuSearchService.$inject = ['$http', '$q'];
   function MenuSearchService($http, $q) {
     var service = this;
-    service.getMatchedMenuItems = function(name, searchTerm, short_name, description) {
-      var deferred = $q.defer();
+    service.getMatchedMenuItems = function(searchTerm, name, short_name, description) {
       var foundItems = [];
       var result =  $http({
         method: "GET",
@@ -64,34 +50,14 @@
       }).then(function (result) {
           var items = result.data;
           for (var i = 0; i < items.menu_items.length; i++) {
-            if (searchTerm === "" || items.menu_items[i].name.toLowerCase().indexOf(searchTerm.toLowerCase()) === -1){
-              if (i === 218 || searchTerm === "") {
-                deferred.reject(["Nothing found"]);
-              }else {
-              }
-            }
-            else if (items.menu_items[i].name.toLowerCase().indexOf(searchTerm.toLowerCase()) ==! -1){
-                foundItems.push({name: items.menu_items[i].name, shortName: items.menu_items[i].short_name, description: items.menu_items[i].description})
-                deferred.resolve(foundItems);
-            }else {
+            if (items.menu_items[i].name.toLowerCase().indexOf(searchTerm.toLowerCase()) > -1 && searchTerm.length > 0){
+              foundItems.push({name: items.menu_items[i].name, shortName: items.menu_items[i].short_name, description: items.menu_items[i].description});
+            }else{
               console.log("doesn't match search");
-            }
+            };
           };
-
       });
-      return deferred.promise;
-    };
-    service.FoundItems = function (searchTerm, name) {
-      var foundArray = [];
-      var searchPromise = service.getMatchedMenuItems(name, searchTerm);
-      foundArray.$promise = searchPromise
-      .then(function (foundItems) {
-        return $q.resolve(foundItems)
-      })
-      .catch(function (errorResponse) {
-          return $q.reject(errorResponse);
-      });
-      return foundArray;
+      return foundItems;
     };
     service.removeItem = function (itemIndex, items) {
       items.splice(itemIndex, 1);
