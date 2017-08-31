26f497541566b06a59903c90d4572cdb4facc8bb
Amon22B <luca.ciancanelli1785@gmail.com>
2017-08-30 21:07:51 -0600

    progress

--- mod4-solution/menudata.service.js
+++ mod4-solution/menudata.service.js
@@ -1,5 +1,28 @@
 (function () {
   'use strict'
   angular.module('MenuData', [])
+  .service('MenuDataService', []);
+
+  MenuDataService.$inject = ['$http'];
+  MenuDataService = function($http) {
+    var service = this;
+    service.getAllCategory = function() {
+      var result =  $http({
+        method: "GET",
+        url: ('https://davids-restaurant.herokuapp.com/menu_items.json'),
+      });
+      return result;
+    }
+    service.getItemsForCategory = function(categoryShortName) {
+      var result =  $http({
+        method: "GET",
+        url: ('https://davids-restaurant.herokuapp.com/menu_items.json'),
+        params{
+          category: categoryShortName
+        }
+      });
+      return result;
+    }
+  };
 
 });
