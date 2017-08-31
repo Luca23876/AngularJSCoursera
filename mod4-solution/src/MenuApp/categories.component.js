2d3bf39710041bbf64a74da222a434ceb5b393cb
Luca23876 <luca.ciancanelli1785@gmail.com>
2017-08-31 16:10:21 -0600

    progress

---
+++ mod4-solution/src/MenuApp/categories.component.js
@@ -0,0 +1,10 @@
+(function() {
+  angular.module('categories')
+  .component('categories'{
+    templateUrl: 'src/MenuApp/template/categories.template.html',
+    bindings: {
+      items: '<'
+    }
+  })
+
+}());
---
+++ mod4-solution/src/MenuApp/items.component.js
@@ -0,0 +1,7 @@
+(function() {
+  angular.module('items')
+  .component('items'{
+
+  })
+
+}());
